306fdcfe98d4b2809fe763ffccddb9df2d6e131a
Matthew <matthew@thespainreport.com>
2025-05-28 16:33:44 +0200

    Basic home page

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-6 rounded-xl shadow-lg bg-white text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Matthew.es</h1>
+        <p className="text-gray-600">Coming soon...</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
